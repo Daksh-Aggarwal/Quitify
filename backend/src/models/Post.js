5af10ac1fb80fb089bfc9044682d8210554d3297
Daksh-Aggarwal <dakshaggarwal2006@gmail.com>
2025-04-12 12:07:39 +0530

    Defined models

--- backend/src/models/Post.js
+++ backend/src/models/Post.js
@@ -1,4 +1,4 @@
-const PostSchema = new monggose.Schema({
+const PostSchema = new mongoose.Schema({
     authorId: mongoose.Schema.Types.ObjectId,
     content: String,
     createdAt: {type: Date, default: Date.now},
